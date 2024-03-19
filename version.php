18eb8067739a6e41ef54cb3f952f3706cccf1f3a
root <snickser@gmail.com>
2024-03-19 22:49:07 +0300

    18

--- version.php
+++ version.php
@@ -3,8 +3,7 @@
 defined('MOODLE_INTERNAL') or die();
 
 $plugin->version = 2024031906;
-$plugin->requires = 2010112400; // Moodle 2.x
+$plugin->requires = 2023100400;
 $plugin->component = 'enrol_payanyway';
 $plugin->cron      = 600;
 $plugin->release = '1.4.1';
-
