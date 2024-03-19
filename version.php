294917902254257694daa1cf69fd7a907df45b6f
Alex <88129688+Snickser@users.noreply.github.com>
2024-03-19 10:26:13 +0300

    Update version.php

--- version.php
+++ version.php
@@ -6,5 +6,5 @@ $plugin->version = 2015091000;
 $plugin->requires = 2010112400; // Moodle 2.x
 $plugin->component = 'enrol_payanyway';
 $plugin->cron      = 600;
-$plugin->release = '1.4.0';
+$plugin->release = '1.4.1';
 
