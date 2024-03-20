06ac827b373e6ed32e01c81ae89514acb90352a5
root <snickser@gmail.com>
2024-03-20 07:49:29 +0300

    new version

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') or die();
 
-$plugin->version = 2024031906;
+$plugin->version = 2024032001;
 $plugin->requires = 2023100400;
 $plugin->component = 'enrol_payanyway';
 $plugin->cron      = 600;
-$plugin->release = '1.4.1';
+$plugin->release = '1.5.0';
